b8541a5fdf2fe4023dbd70bcf752f9a19c9c6684
PolzPT <paultracz07@gmail.com>
2021-10-13 19:30:57 -0400

    EditWorkout activity changed to account for two versions of the xml file

--- app/src/main/java/org/vosk/demo/EditExerciseActivity.java
+++ app/src/main/java/org/vosk/demo/EditExerciseActivity.java
@@ -35,7 +35,7 @@ public class EditExerciseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ativity_workout_exercise);
+        setContentView(R.layout.ativity_edit_workout_exercise);
 
 
         exerciseName = findViewById(R.id.exercise_name);
